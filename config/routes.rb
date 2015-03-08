f50523066c764606880d1930fad2f9eaede748fc
CC Evans <ccevans1@gmail.com>
2015-03-08 14:13:26 -0400

    how it works section

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-
+ 
   get 'pages/launch'
 
   root 'pages#launch'
 
-  devise_for :users, controllers: { registrations: 'users/registrations', sessions: "users/sessions"}
+  devise_for :users, :controllers => { :registrations => 'users/registrations', :sessions => 'users/sessions'}
 
 
 end
